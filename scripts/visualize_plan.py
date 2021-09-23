4b986cf2b72e226811d6bbf5bdc70a3c859bf0c6
Tea Tusar <tea.tusar@ijs.si>
2021-09-23 10:20:16 +0200

    Fancier category labels

--- scripts/visualize_plan.py
+++ scripts/visualize_plan.py
@@ -337,9 +337,16 @@ def plot_data(path_in, path_out):
             path_out, 'country-{}-{}_category-{}_weights-{}_granularity-{}_viz-XXX.png'.format(
                 info['country'].lower().replace(' ', ''), info['start'], info['category'],
                 info['weights'], info['granularity']))
-        title_info = '{}, cat. {}, {} weights, granularity {}'.format(
+        category_labels = {
+            'm2': 'infections falling steeply',
+            'm1': 'infections falling',
+            '0': 'infections steady',
+            '1': 'infections rising',
+            '2': 'infections rising steeply'
+        }
+        title_info = '{}, {}, {} weights, granularity {}'.format(
             get_country_date_string(info['country'], info['start']),
-            info['category'].replace('m', '-'), info['weights'].replace('gdp', 'GDP'),
+            category_labels[info['category']], info['weights'].replace('gdp', 'GDP'),
             info['granularity']
         )
         # Plot of the objective space
